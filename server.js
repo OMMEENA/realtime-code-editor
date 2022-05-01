300a45e1c5f56b935d714440e9c68f23d6b1cf36
Paryank Namdeo <sunny.namdeo14@gmail.com>
2022-05-02 00:29:11 +0530

    Completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const {Server} = require('socket.io');
 const ACTIONS = require("./src/Action");
 
@@ -8,6 +9,11 @@ const ACTIONS = require("./src/Action");
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const userSocketMap = {};
 
