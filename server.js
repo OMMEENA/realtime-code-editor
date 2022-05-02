d27d86cc63ad5728fe338fc44d93f672c27b10f9
Paryank Namdeo <sunny.namdeo14@gmail.com>
2022-05-02 23:17:45 +0530

    minor change

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ io.on('connection', (socket) => {
 });
 
 //console.log("hello");
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
